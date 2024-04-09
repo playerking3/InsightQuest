358b04117d1c167efca2c29588224b42361024ca
Miguel Garrocini <miguelitoplayer1@gmail.com>
2024-04-09 14:18:06 -0300

    tela jogo design

--- InsightQuest/screens/Jogo.js
+++ InsightQuest/screens/Jogo.js
@@ -3,25 +3,37 @@ import {Header} from "@react-navigation/stack";
 import InputTexto from "../components/InputTexto";
 import Btn from "../components/Btn";
 import Imagem from "../components/Imagem";
+import Fundo from "../components/Fundo";
 export default function ({navigation}){
     const css = StyleSheet.create({
         main:{
-            width:'100%'
+            width:'100%',
+            height:'100%',
+            display:"flex",
+            justifyContent:"center",
+            gap:100
         },
         divinp:{
             gap:35,
             display:'flex',
             alignItems:"center",
             width:'100%'
+        },
+        divimg:{
+
         }
     })
     return(
-        <SafeAreaView style={css.main}>
-            <Imagem></Imagem>
-            <View style={css.divinp}>
-                <InputTexto texto={'Isira o conteúdo da imagem aqui'}></InputTexto>
-                <Btn escrita ={'ENVIAR'} navigation={navigation}>0</Btn>
+        <Fundo>
+            <View style={css.main}>
+                <View style={css.divimg}>
+                    <Imagem></Imagem>
+                </View>
+                <View style={css.divinp}>
+                    <InputTexto texto={'Isira o conteúdo da imagem aqui'}></InputTexto>
+                    <Btn escrita ={'ENVIAR'} navigation={navigation}>0</Btn>
+                </View>
             </View>
-        </SafeAreaView>
+        </Fundo>
     )
 }
