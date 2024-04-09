f559654e88ee4fa9ea302010d9dee78014114397
nicolasdosantos <104533715+nicolasdosantos@users.noreply.github.com>
2024-04-09 13:51:41 -0300

    Components pRNT

--- InsightQuest/screens/Inicio.js
+++ InsightQuest/screens/Inicio.js
@@ -1,6 +1,30 @@
-import {Text, View} from "react-native";
+import {ImageBackground, SafeAreaView, Text, View} from "react-native";
 import Header from "../components/Header";
+import Foto from "../components/Foto";
+import InputTexto from "../components/InputTexto";
+import Btn from "../components/Btn";
 
 export default function (){
-    return(<Header></Header>)
+    return(
+        <SafeAreaView>
+            <ImageBackground>
+                <Header></Header>
+                <View>
+                    <Foto></Foto>
+                </View>
+                <View>
+                    <Imagem></Imagem>
+                </View>
+                <View>
+                    <View>
+                        <InputTexto></InputTexto>
+                    </View>
+                    <View>
+                        <Btn></Btn>
+                    </View>
+                </View>
+            </ImageBackground>
+
+        </SafeAreaView>
+    )
 }
