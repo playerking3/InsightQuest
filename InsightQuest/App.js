a33ab9fa682abf9b619bc6300c144e5d40157b2a
nicolasdosantos <104533715+nicolasdosantos@users.noreply.github.com>
2024-04-09 09:42:47 -0300

    --------------

--- InsightQuest/App.js
+++ InsightQuest/App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {NavigationContainer} from "@react-navigation/native";
+import {StatusBar } from 'react-native';
+import Home from "./screens/Home";
+import Inicio from "./screens/Inicio";
+import Jogo from "./screens/Jogo";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+        <NavigationContainer>
+          <StatusBar/>
+          <Stack.Navigator initialRouteName={'Inicio'} screenOptions={{ headerShown: false}}>
+            <Stack.Screen name="Inicio" component={Inicio}/>
+            <Stack.Screen name="Home" component={Home}/>
+            <Stack.Screen name="Jogo" component={Jogo}/>
+          </Stack.Navigator>
+        </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
