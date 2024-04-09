8032867d6669c3962f90222ce13b075847480d64
Miguel Garrocini <miguelitoplayer1@gmail.com>
2024-04-09 14:39:22 -0300

    estilo icones

--- InsightQuest/components/Fundo.js
+++ InsightQuest/components/Fundo.js
@@ -3,7 +3,7 @@ import {ImageBackground, SafeAreaView, View} from "react-native";
 export default function ({children}){
     return(
         <SafeAreaView>
-            <ImageBackground style={{height: "100%"}} resizeMode="cover" source={require("../assets/fundo.jpg")} >
+            <ImageBackground style={{height: "100%" }} resizeMode="cover" source={require("../assets/fundo.jpg")} >
                 {children}
             </ImageBackground>
         </SafeAreaView>
